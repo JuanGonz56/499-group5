f386bc545871a0885fc0d4bc839a8ade43a0319f
adrivnlopez <adrivnlopez@csu.fullerton.edu>
2024-10-01 20:06:06 -0700

    changed port

--- index.js
+++ index.js
@@ -14,4 +14,4 @@ const mongoConnect = async () => {
 
 mongoConnect();
 
-app.listen(3000, () => console.log(" server started on port 3000"));
+app.listen(3007, () => console.log(" server started on port 3007"));
